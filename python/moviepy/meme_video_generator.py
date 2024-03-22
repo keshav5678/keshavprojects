264480572c0baee30d1f6136875ae8109eb55c03
keshav5678 <127648309+keshav5678@users.noreply.github.com>
2024-03-22 11:52:52 +0530

    Update and rename main.py to meme_video_generator.py

--- python/moviepy/meme_video_generator.py
+++ python/moviepy/meme_video_generator.py
@@ -1,7 +1,11 @@
+"""
+# Meme Video Generator
+a project to create videos that have two memes and a background.
+uses meme-api.com to fetch memes and moviepy to compile them into a video.
+"""
+
 import requests
 from moviepy.editor import *
-import tkinter as tk
-from tkinter import ttk
 import sys
 
 img_files = [] 
@@ -14,7 +18,7 @@ resp_json = resp.json()
 
 # save memes in the memes folder
 for meme in resp_json["memes"]:
-    file_name = f'C:\\Users\\Keshav\\keshav_projects_4\\meme_video_generator\\memes\\{ meme["url"].split("/")[-1] }'
+    file_name = f'memes\\{ meme["url"].split("/")[-1] }'
     img_files.append(file_name)
 
     img = requests.get(meme["url"])
@@ -27,7 +31,7 @@ for meme in resp_json["memes"]:
         exit()
 
 # moviepy work
-background = VideoFileClip("C:\\Users\\Keshav\\keshav_projects_4\\meme_video_generator\\videos\\background.mp4").subclip(0,5)
+background = VideoFileClip("videos\\background.mp4").subclip(0,5)
 img_1 = ImageClip(img_files[0])
 img_2 = ImageClip(img_files[1])
 img_1 = img_1.set_position(("center", "top"))
@@ -41,13 +45,7 @@ img_2 = img_2.resize(width=1000)
 video = CompositeVideoClip([background, img_1.set_duration(5), img_2.set_duration(5)])
 if not sys.argv[1]:
     video_name = input("enter video file name: ") + ".mp4"
-    video_name = f"C:\\Users\\Keshav\\keshav_projects_4\\meme_video_generator\\videos\\{video_name}"
+    video_name = f"videos\\{video_name}"
 else:
     video_name = sys.argv[1]
 video.write_videofile(video_name, threads=2)
-
-
-
-
-
-
