838dd156f939ee6d36304ba6b5c427b9ae67ce3e
keshav5678 <127648309+keshav5678@users.noreply.github.com>
2024-03-22 12:25:05 +0530

    Update config.py

--- python/flask/multiplayer_trivia_game/config.py
+++ python/flask/multiplayer_trivia_game/config.py
@@ -4,7 +4,7 @@ from flask import Flask
 from flask_socketio import SocketIO
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'aa00dfce3194ecec551ec7fc9dcaf1273eb27c1387b5412d3151c7b4cdee31f8'
+app.config['SECRET_KEY'] = 'dev' # please change this if you want to run this yourself.
 socketio = SocketIO(app)
 
 the_rooms = dict()
